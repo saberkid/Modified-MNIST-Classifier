4f1d51f95a51af6b54371eb7e816bcb29fb03e06
saberkid <saberkid@users.noreply.github.com>
2017-11-03 17:54:31 -0400

    Update data_reader.py

--- CNN/data_reader.py
+++ CNN/data_reader.py
@@ -2,10 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+    
 class Reader:
     def __init__(self,train_file, test_file):
         self.train_file = train_file
         self.test_file = test_file
+        
+        self.num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81]
+        self.mapping = {}
+        for index,item in enumerate(self.num_list):
+            onehot = [0]*40
+            onehot[index] = 1
+            self.mapping[item] = onehot
 
     def vectorizer(self, line):
         vector = []
@@ -24,7 +32,7 @@ class Reader:
                 (label, line) = line.split(",")[0], line.split(",")[1]
                 line_vector = self.vectorizer(line)
                 X_train.append(line_vector)
-                y_train.append(int(label))
+                y_train.append(self.mapping[int(label)])
                 # X_train, y_train = np.asarray(X_train), np.asarray(y_train)
                 # x = X_train[0].reshape(64, 64)  # reshape
                 # y = y_train[0]
@@ -47,7 +55,7 @@ class Reader:
                 (label, line) = line.split(",")[0], line.split(",")[1]
                 line_vector = self.vectorizer(line)
                 X_val.append(line_vector)
-                y_val.append(int(label))
+                y_val.append(self.mapping[int(label)])
         X_val, y_val = np.asarray(X_val), np.asarray(y_val)
         return X_val, y_val
 
